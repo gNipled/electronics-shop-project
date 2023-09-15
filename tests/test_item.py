d4ca470ad7e6daad7887ca1edceaf7734e289116
Aleksei Ponomarenko <lehapon92@gmail.com>
2023-09-15 17:39:45 +0900

    added tests for new methods, test coverage is 80%

--- tests/test_item.py
+++ tests/test_item.py
@@ -15,3 +15,16 @@ def test_apply_discount():
     item2.apply_discount()
     assert item1.price == 500.0
     assert item2.price == 1000.0
+
+
+def test_initiate_from_csv():
+    Item.instantiate_from_csv('src/items.csv')
+    assert len(Item.all) == 5
+    item1 = Item.all[0]
+    assert item1.name == 'Смартфон'
+
+
+def test_string_to_number():
+    assert Item.string_to_number('5') == 5
+    assert Item.string_to_number('5.0') == 5
+    assert Item.string_to_number('5.5') == 5
