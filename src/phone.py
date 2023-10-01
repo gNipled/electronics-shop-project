f2f2555f5cb55b10bd86265057ff2382f12c7710
Aleksei Ponomarenko <lehapon92@gmail.com>
2023-10-01 18:23:20 +0900

    added Phone class and wrote tests for it

---
+++ src/phone.py
@@ -0,0 +1,27 @@
+from src.item import Item
+
+
+class Phone(Item):
+    def __init__(self, name: str, price: float, quantity: int, number_of_sim: int):
+        super().__init__(name, price, quantity)
+        if number_of_sim <= 0 or not isinstance(number_of_sim, int):
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        else:
+            self.number_of_sim = number_of_sim
+
+    def __str__(self):
+        return f'{self.name}'
+
+    def __repr__(self):
+        return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+
+    @property
+    def number_of_sim(self):
+        return self.__number_of_sim
+
+    @number_of_sim.setter
+    def number_of_sim(self, value: int):
+        if value <= 0 or not isinstance(value, int):
+            raise ValueError('Количество физических SIM-карт должно быть целым числом больше нуля.')
+        else:
+            self.__number_of_sim = value
