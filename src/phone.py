327f15b3e88d80bb72d149b2402fd1532167c32e
Aleksei Ponomarenko <lehapon92@gmail.com>
2023-10-01 21:44:24 +0900

    added keyboard class +minor fixes in phone.py

--- src/phone.py
+++ src/phone.py
@@ -13,7 +13,7 @@ class Phone(Item):
         return f'{self.name}'
 
     def __repr__(self):
-        return f"Phone('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
+        return f"{self.__class__.__name__}('{self.name}', {self.price}, {self.quantity}, {self.number_of_sim})"
 
     @property
     def number_of_sim(self):
