c11f171a64f6e6c5dbe32dbd43c3febbe89e91df
Aleksei Ponomarenko <lehapon92@gmail.com>
2023-10-04 20:29:48 +0900

    Added exceptions to instantiate_from_csv function with custom exception

---
+++ src/Exceptions.py
@@ -0,0 +1,9 @@
+import csv
+
+
+class InstantiateCSVError(Exception):
+    def __init__(self, *args, **kwargs):
+        self.message = args[0] if args else 'Файл item.csv поврежден'
+
+    def __str__(self):
+        return self.message
